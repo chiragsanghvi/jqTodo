9cd35fa6b7379e09373c67c333727d067a269431
csanghvi <csanghvi@tavisca.com>
2013-02-04 14:41:47 +0530

    removed some files

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-$(function () {
-	Appacitive.session.environment = 'live';
-	Appacitive.session.create({
-		apikey: 'ukaAo61yoZoeTJsGacH9TDRHnhf/J9/kH2TStR5sD3k='
-	});
-});
-
 Appacitive.eventManager.subscribe('session.success', function() {
 	if ($) {
 		$('#btnLogin').attr('disabled', false).attr('value', 'Log In');
